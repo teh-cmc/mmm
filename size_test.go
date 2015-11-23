4428e36f14e011d2b361d1d56e92a10f9510d732
cmc <cr.rey.clement@gmail.com>
2015-11-23 17:42:14 +0100

    added TestSize_SizeOf_int

--- size_test.go
+++ size_test.go
@@ -17,3 +17,37 @@ func TestSize_SizeOf_bool(t *testing.T) {
 		t.Error("invalid size for bool")
 	}
 }
+
+func TestSize_SizeOf_int(t *testing.T) {
+	var v int
+	size, err := SizeOf(v)
+	if err != nil {
+		t.Error(err)
+	}
+	if size != unsafe.Sizeof(v) {
+		t.Error("invalid size for bool")
+	}
+}
+
+//var i8 int8
+//var i16 int16
+//var i32 int32
+//var i64 int64
+//var ui uint
+//var ui8 uint8
+//var ui16 uint16
+//var ui32 uint32
+//var ui64 uint64
+//var uiptr uintptr
+//var f32 float32
+//var f64 float64
+//var c64 complex64
+//var c128 complex128
+//var iarr [42]int
+//var ichan chan int
+//var itf interface{}
+//var imap map[int]int
+//var iptr *int
+//var islice []int
+//var str string
+//var srt struct{}
