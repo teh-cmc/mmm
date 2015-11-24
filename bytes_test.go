66c727fb8bb26ec924a3df5877a3cd80d31de2bf
cmc <cr.rey.clement@gmail.com>
2015-11-24 12:29:00 +0100

    added TestBytes_BytesOf_int

--- bytes_test.go
+++ bytes_test.go
@@ -21,3 +21,18 @@ func TestBytes_BytesOf_bool(t *testing.T) {
 		t.Error("invalid bytes for bool")
 	}
 }
+
+func TestBytes_BytesOf_int(t *testing.T) {
+	var v int = 42
+	size := unsafe.Sizeof(v)
+	bytes := make([]byte, size)
+
+	if err := BytesOf(v, bytes); err != nil {
+		t.Error(err)
+	}
+
+	rv := *((*int)(unsafe.Pointer(&(bytes[0]))))
+	if v != rv {
+		t.Error("invalid bytes for bool")
+	}
+}
