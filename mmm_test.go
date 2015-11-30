6f687ce6ebb72b0c6b9234317a650afa135f6ac0
cmc <cr.rey.clement@gmail.com>
2015-11-30 10:03:43 +0100

    updated package example

--- mmm_test.go
+++ mmm_test.go
@@ -17,20 +17,26 @@ type Coordinate struct {
 	x, y int
 }
 
-func Example() {
+func Example_simple_usage() {
 	// create a new memory chunk that contains 3 Coordinate structures
 	mc, err := NewMemChunk(Coordinate{}, 3)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// print 3
 	fmt.Println(mc.NbObjects())
 
+	// write {3,9} at index 0, then print {3,9}
 	fmt.Println(mc.Write(0, Coordinate{3, 9}))
+	// write {17,2} at index 1, then print {17,2}
 	fmt.Println(mc.Write(1, Coordinate{17, 2}))
+	// write {42,42} at index 2, then print {42,42}
 	fmt.Println(mc.Write(2, Coordinate{42, 42}))
 
+	// print {17,2}
 	fmt.Println(mc.Read(1))
+	// print {42,42}
 	fmt.Println(*((*Coordinate)(unsafe.Pointer(mc.Pointer(2)))))
 
 	// free memory chunk
