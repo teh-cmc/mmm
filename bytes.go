8a7e776c4b8bdfbb36cc22d0fd9309da5228bb8c
lukechampine <luke.champine@gmail.com>
2016-05-21 03:06:14 -0400

    use slice trick to copy underlying bytes

--- bytes.go
+++ bytes.go
@@ -32,12 +32,8 @@ func bytesOf(v interface{}, bytes []byte) error {
 	}
 
 	switch t {
-	case TypeNumeric:
-		return bytesOfNumericType(v, bytes)
-	case TypeArray:
+	case TypeNumeric, TypeArray, TypeStruct:
 		return bytesOfArrayType(v, bytes)
-	case TypeStruct:
-		return bytesOfStructType(v, bytes)
 	case TypeUnsafePointer:
 		// do nothing
 	}
@@ -47,47 +43,25 @@ func bytesOf(v interface{}, bytes []byte) error {
 
 // -----------------------------------------------------------------------------
 
-func extractDataAddress(v interface{}) uintptr {
-	// extract data address from the interface
-	vbytes := *((*[unsafe.Sizeof(v)]byte)(unsafe.Pointer(&v)))
-	vuintptr := *(*uintptr)(unsafe.Pointer(&(vbytes[unsafe.Sizeof(v)-unsafe.Sizeof(uintptr(0))])))
-
-	return vuintptr
-}
-
-func bytesOfNumericType(v interface{}, bytes []byte) error {
-	// interpret data as a contiguous array of 16 bytes
-	vvalue := *((*[16]byte)(unsafe.Pointer(extractDataAddress(v))))
-	// copy data byte-representation to `bytes`
-	copy(bytes, vvalue[:reflect.ValueOf(v).Type().Size()])
-
-	return nil
-}
-
 func bytesOfArrayType(v interface{}, bytes []byte) error {
 	if err := TypeCheck(v); err != nil {
 		return err
 	}
 
-	vuintptr := extractDataAddress(v)
-	size := reflect.ValueOf(v).Type().Size()
-	// loop until there are no more data chunks
-	for i := uintptr(0); size > 0; i += 128 {
-		// interpret data as a contiguous array of 128 bytes
-		vvalue := *((*[128]byte)(unsafe.Pointer(vuintptr + i)))
-		// copy data byte-representation to `bytes`
-		if size > 128 {
-			copy(bytes[i:], vvalue[:128])
-			size -= 128
-		} else {
-			copy(bytes[i:], vvalue[:size])
-			size = 0
-		}
-	}
+	// First extract v's data pointer and determine its size.
+	vuintptr := (*[2]uintptr)(unsafe.Pointer(&v))[1]
+	size := reflect.TypeOf(v).Size()
+
+	// Construct a "fake slice" using the data pointer and size. This object's
+	// memory layout will match that of []byte.
+	slice := struct {
+		data uintptr
+		_len uintptr
+		_cap uintptr
+	}{vuintptr, size, size}
+
+	// Convert the fake slice to a real byte slice and copy it.
+	copy(bytes, *(*[]byte)(unsafe.Pointer(&slice)))
 
 	return nil
-}
-
-func bytesOfStructType(v interface{}, bytes []byte) error {
-	return bytesOfArrayType(v, bytes)
 }
